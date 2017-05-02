954edc18ce884e8d3301d5e52667bd1d0fa81d91
Renan Lopes <renanlopescoder@gmail.com>
2017-05-02 17:12:16 -0300

    feat: total volume
    - Adicionado dinamicamente o total de volume usando os novos padrões de projeto do ES6 com separação de responsabilidades da view método reduce com arrow function na template string.

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -9,6 +9,29 @@
      * @param {array} Array de Negociacoes
      */
 
+    /**
+     * No tbody usamos o map para varrer todas as posições do array, como padrão das arrow functions do ES6 não necessitamos inserir os parênteses para apenas um parâmetro, também tiramos as chaves e o return pois estamos passando apenas uma instrução.
+     */
+
+    /**
+     * Para contagens do total do volume "<tfoot>" uma das soluções é criar uma função dentro da expressão na template string qual se executa, para isso usamos funções que se chamam Immediately-Invoked Function Expression (IIFE)
+     * 
+     * Ex.: 
+     * 
+     *  ${
+          (function(){
+            let total = 0;
+            model.negociacoes.forEach(negociacao => total += negociacao.volume);
+            return total;
+          })()
+        }
+      * 
+      * Atualizando para o paradigma funcional a solução seria usar um método do array chamado reduce para retornar apenas um resultado, para inicializar o total passamos 0.0 como segundo parâmetro do método reduce.
+
+      Ex.: 
+        ${model.negociacoes.reduce( (total, negociacao) => total + negociacao.volume, 0.0)}
+     */
+
     _template(model) {
       return `
           <table class="table table-hover table-bordered">
@@ -22,19 +45,25 @@
             </thead>
             
             <tbody>
-              ${model.negociacoes.map( n => {
-                return `
+              ${model.negociacoes.map( negociacao => `
+
                   <tr>
-                    <td>${DateHelper.dataParaTexto(n.data)}</td>
-                    <td>${n.quantidade}</td>
-                    <td>${n.valor}</td>
-                    <td>${n.volume}</td>
-                  </tr
-                `;
-              }).join('')}
+                    <td>${DateHelper.dataParaTexto(negociacao.data)}</td>
+                    <td>${negociacao.quantidade}</td>
+                    <td>${negociacao.valor}</td>
+                    <td>${negociacao.volume}</td>
+                  </tr>
+
+              `).join('')}
             </tbody>
             
             <tfoot>
+              <td colspan="3"></td>
+              <td>
+                ${model.negociacoes.reduce( (total, negociacao) => {
+                  return total + negociacao.volume;
+                }, 0.0)}
+              </td>
             </tfoot>
         </table>
       `;
