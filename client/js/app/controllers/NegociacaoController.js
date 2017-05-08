28f7a82e5477b097ccc928b408f1862705ab4666
Renan Lopes <renanlopescoder@gmail.com>
2017-05-08 11:17:29 -0300

    feat: apagar negociações e melhorias no update
    - Adicionado conceitos de escopo de uma função (dinâmico) e Arrow Function (Escopo léxico) e as tratativas nos comentários
    - Adicionado recurso de limpar negociações
    - Melhorias encapsulando o update evitando sua chamada em diversas funções

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,12 +3,12 @@ class NegociacaoController {
   /**
    * Construtor acessa o DOM para evitar que as funções
    * necessitem acessar a todo momento de sua execução
-   * temos melhora na performance.
+   * assim temos melhora na performance.
    * 
    * Essa é uma boa estratégia, realizar uma espécie de cache do 
    * elemento do DOM que você deseja manipular, ou seja, guardar
    * sua referência, para que você o busque apenas uma vez.
-   */
+  */
 
   constructor () {
 
@@ -22,45 +22,65 @@ class NegociacaoController {
      * 
     */
 
-    let $ = document.querySelector.bind(document);
+      let $ = document.querySelector.bind(document);
 
-    this._inputData = $('#data');
-    this._inputQuantidade = $('#quantidade');
-    this._inputValor = $('#valor');
-    this._listaNegociacoes = new ListaNegociacoes();
-    this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-    this._negociacoesView.update(this._listaNegociacoes);
-    this._mensagem = new Mensagem();
-    this._mensagemView = new MensagemView($('#mensagemView'))
-    this._mensagemView.update(this._mensagem);
+      this._inputData = $('#data');
+      this._inputQuantidade = $('#quantidade');
+      this._inputValor = $('#valor');
+
+    /**
+     * Usando o function quando usamos o 'this' em uma função o contexto é dinâmico ou seja vai alterar de acordo ao contexto onde está sendo usado e como opção podemos usar a API Reflections do JavaScript passando o contexto através do this como parâmetro do construtor e nos métodos de destino usamos o método estatico da classe Reflect.apply(this._trap, this._contexto, [this]); passando como parâmetro a função e o contexto enviado ao construtor e um array com os parametros que irão receber, assim podemos pegar o contexto anterior ao contexto de execuçãodo método.
+     * 
+      this._listaNegociacoes = new ListaNegociacoes(this #Contexto NegociacaoController#, function(model){
+        this._negociacoesView.update(this._listaNegociacoes(model));
+      });
+     * 
+     * Atualização do ES6, quando usamos a arrow function o 'this' possúi escopo léxico e o this passado sempre será o this do escopo atual e não será alterado.
+     */
+
+      this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
+
+      this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+
+      // Primeira renderização da lista
+      this._negociacoesView.update(this._listaNegociacoes);
+      this._mensagem = new Mensagem();
+      this._mensagemView = new MensagemView($('#mensagemView'))
+      this._mensagemView.update(this._mensagem);
   }
 
   adiciona (event) {
 
-    event.preventDefault();
+    event.preventDefault ();
 
-      this._listaNegociacoes.adiciona(this._criaNegociacao());
-      this._negociacoesView.update(this._listaNegociacoes);
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    // this._negociacoesView.update(this._listaNegociacoes);
 
-      this._mensagem.texto = 'Negociação Adicionada com Sucesso';
-      this._mensagemView.update(this._mensagem);
+    this._mensagem.texto = 'Negociação Adicionada com Sucesso';
+    this._mensagemView.update(this._mensagem);
 
-      this._limpaFormulario();
+    this._limpaFormulario();
   }
 
   _criaNegociacao () {
     return new Negociacao(
       DateHelper.textoParaData(this._inputData.value),
       this._inputQuantidade.value,
-      this._inputValor.value);
+      this._inputValor.value
+    );
+  }
+
+  apaga () {
+    this._listaNegociacoes.deletaLista();
+    // this._negociacoesView.update(this._listaNegociacoes);
+    this._mensagem.texto = 'Negociações Apagadas com Sucesso';
+    this._mensagemView.update(this._mensagem);
   }
 
   _limpaFormulario () {
     this._inputData.value = '';
     this._inputQuantidade.value = 1;
     this._inputValor.value = 0.0;
-
     this._inputData.focus();
   }
-
 }
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,11 +1,33 @@
 class ListaNegociacoes {
 
-  constructor(){
+/**
+ * Trap é uma 'armadilha' que será executada ao chamar as funções irá atualizar a view caso tenha alguma alteração no modelo.
+ * 
+ * Caso usamos uma função de contexto dinâmico usando a API Reflect do JavaScript para pegar escopo anterior ao de execução passando o contexto devemos receber o contexto no construtor.
+ * @param {function} trap
+ * 
+ * constructor (contexto, trap) {
     this._negociacoes = [];
+    this._trap = trap;
+    this._contexto = contexto;
+    }
+ */
+
+/** Usando a Arrow function não precisamos do contexto pois a arrow function possui escopo léxico */
+  constructor (trap) {
+    this._negociacoes = [];
+    this._trap = trap;
   }
 
   adiciona(negociacao) {
     this._negociacoes.push(negociacao);
+    this._trap(this); /*Contexto NegociacaoController (Contexto Léxico da Arrow Function)*/
+    /**
+     * Mais Informações para entendimento no construtor da classe NegociacaoController
+     */
+    /**
+     * Reflect.apply(this._trap #Contexto ListaNegociacoes#, this._contexto #Contexto NegociacoesController por causa do Reflect.apply#, [this] #Modelo com ListaNegociacoes#);
+     */
   }
 
 /**
@@ -14,7 +36,13 @@ class ListaNegociacoes {
  * Para evitar que o desenvolvedor possa acessar o array fora da classe diretamente temos que passa um novo array adicionando todas as informações existentes no array original com o a função concat, assim se o array for alterado fora da classe será apenas uma cópia do nosso array original.
  */
 
-  get negociacoes (){
+  get negociacoes () {
     return [].concat(this._negociacoes);
+  }
+
+  deletaLista () {
+    this._negociacoes = [];
+    this._trap(this); /*Contexto NegociacaoController (Contexto Léxico da Arrow Function)*/
+    // Reflect.apply(this._trap, this._contexto, [this]);
   }
 }
